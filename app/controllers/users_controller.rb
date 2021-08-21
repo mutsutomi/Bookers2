cea4aa36f56684b9a1857f3a0d86ada35ff90094
mutsutomi <m2.tmk@ezweb.ne.jp>
2021-08-21 16:19:27 +0000

    step27

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "about" => "homes#about"
   devise_for :users
-  resources :books, only: [:index, :show]
+  resources :books, only: [:index, :show, :new, :create]
   resources :users, only: [:index, :show, :edit]
  end
