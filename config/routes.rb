1cd4746a00c7406654edc753f4c0637de0490180
mutsutomi <m2.tmk@ezweb.ne.jp>
2021-08-21 08:28:13 +0000

    step19

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
-  resources :books, only: [:index]
-  resources :users, only: [:show]
+  resources :books, only: [:index, :show]
+  resources :users, only: [:index, :show, :edit]
  end
