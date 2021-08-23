aa7efd116db05fe7efdb2d5a8811c441d9d3b032
mutsutomi <m2.tmk@ezweb.ne.jp>
2021-08-23 10:06:49 +0000

    step37

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c900fbaa58dc9914fc32c172fd628aa350dd2e9e19b6743b1e61a977799334e39d357fb714d0dc3dd08441c487e14b5ac67f020de22140d1a69632f97b4a110c'
