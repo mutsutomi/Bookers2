c839d3fdaabb984fb229581e2e48cda2f98a43a6
mutsutomi <m2.tmk@ezweb.ne.jp>
2021-08-23 09:35:34 +0000

    step35

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "about" => "homes#about"
   devise_for :users
-  resources :books, only: [:index, :show, :create, :edit]
+  resources :books, only: [:index, :show, :create, :edit, :update]
   delete "books/:id" => "books#destroy", as: "destroy_book"
   resources :users, only: [:index, :show, :edit, :update]
  end
