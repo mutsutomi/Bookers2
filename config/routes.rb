ecd537bb1395ae1375cf5618e5dec27514439814
mutsutomi <m2.tmk@ezweb.ne.jp>
2021-08-26 11:16:29 +0000

    addComment

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   get "home/about" => "homes#about"
   devise_for :users
   resources :books, only: [:index, :show, :create, :edit, :update] do
+    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-  
+
   delete "books/:id" => "books#destroy", as: "destroy_book"
   resources :users, only: [:index, :show, :edit, :update]
  end
